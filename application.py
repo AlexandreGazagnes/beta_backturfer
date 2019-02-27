086b2eab53bf6ff9c1adb9e5119b56bcc5a28b23
AlexandreGazagnes <a.gazagnes@gmail.com>
2019-02-27 12:43:25 +0100

    working on a web app

--- application.py
+++ application.py
@@ -7,6 +7,10 @@ from flask import Flask, render_template, request
 # init
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/turfing", methods=["GET", "POST"])
+def turfing():
+    return render_template("turfing.html")
