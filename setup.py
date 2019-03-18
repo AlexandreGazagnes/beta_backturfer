b315a3e382892388c290caf7ae12d78b5d75868b
AlexandreGazagnes <a.gazagnes@gmail.com>
2019-03-18 16:09:44 +0100

    upgrade setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
 from setuptools import setup, find_packages
 
-install_requires = [
-    'pandas',
-    'pyarrow >= 0.12.1',
-    'tqdm >= 4.31.1',
-]
+install_requires = [    'pandas',
+                        'numpy',
+                        'matplotlib', 
+                        'seaborn',
+                        'tqdm',
+                        'flask',
+                        'flask_session',
+                        'flask_wtf', 
+                        'wtforms',
+                        'bs4',     ]
 
-setup(
-    name='beta_backturfer',
-    version='0.1',
-    packages=find_packages(),
-    author='Alexandre Gazagnes',
-    author_email='a.gazagnes@gmail.com',
-    description='An easy to use library to try various back turfing strats',
-    long_description=open('README.md').read(),
-    url='https://github.com/alexandregazagnes/beta_backturfer',
-    install_requires=install_requires,
-    license='GNU',
-)
+
+setup(  name            = 'beta_backturfer',
+        version         = '0.1',
+        packages        = find_packages(),
+        author          = 'Alexandre Gazagnes',
+        author_email    = 'a.gazagnes@gmail.com',
+        description     = 'An easy to use library to try various back turfing strats',
+        long_description= open('README.md').read(),
+        url             = 'https://github.com/AlexandreGazagnes/beta_backturfer',
+        install_requires= install_requires,
+        license         = 'GNU'     )
 
