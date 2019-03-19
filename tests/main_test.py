0a86df581f146123bed3ee20ee6cf1cd50b2ff31
AlexandreGazagnes <a.gazagnes@gmail.com>
2019-03-19 17:59:11 +0100

    add GroupBy.internalize_results() test

--- tests/main_test.py
+++ tests/main_test.py
@@ -22,7 +22,7 @@ def test_df_shape() :
 
 
 def test_RaceSelector() : 
-
+    
     df  = pk_load("WITHOUT_RESULTS_pturf_grouped_and_merged_cache_carac_2016-2019_OK", "data/")
     form        = { 'date_start': "2018-01-01", 'quinte' : 1, 
                     'euro_only' : True, 'typec': [  'attelé', 'monté', "plat"]}
@@ -34,4 +34,25 @@ def test_RaceSelector() :
     assert 'url' in df.columns
     assert 'comp' in df.columns
     assert 'results' not in df.columns
+
+
+def test_GroupBy_internalize_results() : 
+
+    from backturfer import * 
+    
+    df  = pk_load("WITHOUT_RESULTS_pturf_grouped_and_merged_cache_carac_2016-2019_OK", "data/")
+    form        = { 'date_start': "2018-01-01", 'quinte' : 1, 
+                    'euro_only' : True, 'typec': [  'attelé', 'monté', "plat"]}
+    race_sel    = RaceSelector(form)
+    df          = race_sel(df)
+
+    df = GroupBy.internalize_results(df)
+
+    assert "results" in df.columns
+    results = df.results.iloc[0]
+    assert isinstance(results, pd.DataFrame)
+    assert "cl" in results.columns
+    assert "numero" in results.columns
+
+
 
