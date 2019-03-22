f5cbc9e7d4e65a98a1f53a4a2bf91d2a22c59961
AlexandreGazagnes <a.gazagnes@gmail.com>
2019-03-22 17:30:09 +0100

    ignore webapp for now

--- backturfer/__init__.py
+++ backturfer/__init__.py
@@ -5,14 +5,7 @@
 from backturfer.misc           import * 
 from backturfer.strats         import *
 from backturfer.model          import * 
-
-# from backturfer.build          import Build
-# from backturfer.groupby        import GroupBy
-# from backturfer.select         import RaceSelector
-# from backturfer.addcote      import CoteSimplePlace, CoteCouple, CoteDeuxSurQuatre, CoteTrio, CoteTierce, CoteQuinte
-# from backturfer.addcote        import AddCote
+# from backturfer.web            import * 
 
 from backturfer.bet            import Bet
 from backturfer.turf           import TurfingRoom
-
-
---
+++ backturfer/web/__init__.py
@@ -0,0 +1,1 @@
+from backturfer.web.webapp import * 
