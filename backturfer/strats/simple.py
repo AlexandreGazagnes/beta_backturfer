28a15953976d386c0a6ca20f4ca9a250556b1391
AlexandreGazagnes <a.gazagnes@gmail.com>
2019-04-02 11:47:53 +0200

    reshape part of strats to be inserted in html form

--- backturfer/strats/simple.py
+++ backturfer/strats/simple.py
@@ -24,6 +24,7 @@ class SimpleStrats(MultiStrats) :
 
 
     # CHOIX PRECIS - MEILLEURS
+    # -------------------------------------
 
     @change_repr
     def choix_de_la_meilleure_cote(results, N=None, n=None, cote_type="direct") : 
@@ -47,6 +48,8 @@ class SimpleStrats(MultiStrats) :
 
 
     # CHOIX PRECIS - PIRES
+    # -------------------------------------
+
     @change_repr
     def choix_de_la_pire_cote_inscrite(results, N=None, n=None, cote_type="direct"):
 
@@ -59,6 +62,7 @@ class SimpleStrats(MultiStrats) :
 
 
     # CHOIX ALEATOIRE PUR
+    # -------------------------------------
 
     @change_repr
     def choix_aleatoire_un_inscrit(results, N=None, n=None, cote_type="direct") : 
@@ -77,6 +81,7 @@ class SimpleStrats(MultiStrats) :
 
 
     # CHOIX ALEATOIRE VS COTES - MEILLEURS
+    # -------------------------------------
 
     @change_repr
     def choix_aleatoire_parmi_les_3_meilleures_cotes(results, N=None, n=None, cote_type="direct") : 
@@ -98,6 +103,7 @@ class SimpleStrats(MultiStrats) :
 
 
     # CHOIX ALEATOIRE VS COTES - PIRES
+    # -------------------------------------
 
     @change_repr
     def choix_aleatoire_parmi_les_3_pires_cotes_inscrites(results, N=None, n=None, cote_type="direct") : 
@@ -115,98 +121,3 @@ class SimpleStrats(MultiStrats) :
     def choix_aleatoire_parmi_les_5_pires_cotes_partantes(results, N=None, n=None, cote_type="direct") : 
         return NotImplementedError("NotImplementedError")
 
-
-
-
-    # # @change_repr
-    # # def choix_aleatoire_parmi_les__N__meilleures_cotes(results, N, n=1, cote_type="direct") : 
-    # #     """chose one horse random given the N best cotes"""
-
-    # #     if not isinstance(N, int) : 
-    # #         raise ValueError("N should be an int")
-
-    # #     if  N == 0 : 
-    # #         return -1
-
-    # #     r = results.sort_values(f"cote{cote_type}", ascending=True, inplace=False)
-    # #     best_N_cotedirect = r.numero.iloc[:N].values
-
-    # #     r = choice(best_N_cotedirect, size=n, replace=False)
-    # #     if len(r) == 1 : 
-    # #         r = r[0]
-
-    # #     return r
-
-
-    # # @change_repr
-    # # def choix_aleatoire_parmi_les_3_meilleures_cotes(results, N=None, n=1, cote_type="direct") : 
-    # #     """chose one horse random given the 3 best cotes"""
-
-    # #     return SimpleStrats.choix_aleatoire_parmi_les__n__meilleures_cotes(results, 3, n, cote_type)
-
-
-    # # # @change_repr
-    # # # def random_1_btwn_n_and_m_best_winning_cote(results, n ,m, cote_type="direct") : 
-    # # #     """chose one horse random between the n+1 and the m+1 rank of best cotes"""
-
-    # # #     r = results.sort_values(f"cote{cote_type}", ascending=True, inplace=False)
-    # # #     best_n_m_cotedirect = r.numero.iloc[n:m].values
-
-    # # #     return choice(best_n_m_cotedirect)
-
-
-    # @change_repr
-    # def choix_de_la__N__meilleure_cote(results, N, n=0, cote_type="direct") : 
-    #     """chose the horse with nth best cote"""
-        
-    #     if not isinstance(N, int) : 
-    #         raise ValueError("N should be an int")
-
-    #     if N >= len(results) : 
-    #         info("N sup >= nb chevaux")
-    #         return -1
-
-    #     r = results.sort_values(f"cote{cote_type}", ascending=True, inplace=False)
-
-    #     return r.numero.iloc[N]
-
-
-    # @change_repr
-    # def choix_de_la_meilleure_cote(results, N=None, n=0, cote_type="direct") : 
-    #     """chose the horse with best cote"""
-
-    #     return SimpleStrats.choix_de_la__N__meilleure_cote(results, 0, n, cote_type)
-
-
-    # @change_repr
-    # def ne_jamais_parier(results, N=None) : 
-    #     """just go to the hippo, no bets, just enjoy races and stalk beautiful ladies"""
-
-    #     return -1
-
-
-    # @change_repr
-    # def pile_ne_pas_jouer_face_jouer_la_meilleure_cote(results, N=None, cote_type="direct") : 
-    #     """go to the hippo, flip a coin and 1/2 decide to bet or not, if bet, use best_cote_strategy"""
-
-    #     n = randint(0, 2)
-    #     if n : return SimpleStrats.choix_de_la__N__meilleure_cote(results, 0, cote_type)
-    #     else : return -1
-
-
-    # @change_repr
-    # def choisir_la_pire_cote_inscrite(results, N=None, cote_type="direct") : 
-    #     """chose the horse with best cote)"""
-
-    #     r = results.sort_values(f"cote{cote_type}", ascending=True, inplace=False)
-
-    #     return r.numero.iloc[-1]
-
-
-    # @change_repr
-    # def choisir_la_pire_cote_partante(results, N=None, cote_type="direct") : 
-    #     """chose the horse with best cote)"""
-
-    #     r = results.sort_values(f"cote{cote_type}", ascending=True, inplace=False)
-
-    #     raise NotImplementedError
